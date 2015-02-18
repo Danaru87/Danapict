dfa112219c6ef97d9ea01e1e3bf9d8bf9c1fc9f3
Arnaud Da Silva <dasilva.arnaud@gmail.com>
2015-02-18 14:41:36 +0100

    Update bdd.fn.php

--- class/bdd.fn.php
+++ class/bdd.fn.php
@@ -1,10 +1,10 @@
 <?php
 	function Connect()
 	{
-	$host ='mysql51-89.perso'; 
-	$user ='othenticdanapict';  
-	$pass = '0150zc41'; 
-	$base ='othenticdanapict';
+	$host =''; 
+	$user ='';  
+	$pass = ''; 
+	$base ='';
 	$db = new mysqli ($host, $user, $pass, $base); 
 	return $db; 
 	}
